2d53d593742ee46e8b9c0741a2a2b39eb8781634
noah <yochibot01@gmail.com>
2011-10-23 18:21:36 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LavalandNew::Application.config.secret_token = '429d2675a9885a78e400bb267929971cfa914da4ccc122007d4932cb2b186a527717b6547f751b0f0e71373506929892a8cd9c059932c34701f7e033404488b0'
